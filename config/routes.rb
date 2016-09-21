0d15e6262be84716a6b7dfdd76e33dfa3550cd8d
kumamonmaster <fuusuke0715@gmail.com>
2016-09-21 08:13:19 +0000

    Add checkbox for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tasks, only: [:create, :destroy]
   end
+
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
 end
