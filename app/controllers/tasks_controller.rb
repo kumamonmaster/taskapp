519acb780af9ba3ee70249ded7c2364fbd3dcaad
kumamonmaster <fuusuke0715@gmail.com>
2016-09-21 07:41:22 +0000

    Make create for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+  def create
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.create(task_params)
+    redirect_to project_path(@project)
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
